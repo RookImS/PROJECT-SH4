62a1308db6176825bf2db0cd91f1e3c04831f85c
RookImS <rookims1996@hotmail.com>
2025-08-31 13:42:21 +0900

    EntityBroadcaster, EntityListener와 관련 인터페이스 제작 및 사용 예시 첨부

---
+++ Assets/Scripts/Core/Common/ObserverExample/ExampleManager1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class ExampleManager1 : MonoBehaviour
+{
+    private IEntityListner<ExampleEntity> listener;
+
+    private void Awake()
+    {
+        listener = EntityListener<ExampleEntity>.Create(new List<Action<ExampleEntity>>{ Print });
+        listener.Subscribe(EntityBroadcaster<ExampleEntity>.Observable);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnDestroy()
+    {
+        listener?.Dispose();
+    }
+
+    private void Print(ExampleEntity entity)
+    {
+        Debug.Log($"[{GetType().Name}]");
+        Debug.Log($"GameObject: {entity.Go.name}");
+        Debug.Log($"IntValue: {entity.IntValue}");
+        Debug.Log($"FloatValue: {entity.FloatValue}");
+        Debug.Log($"StringValue: {entity.StringValue}");
+    }
+}
